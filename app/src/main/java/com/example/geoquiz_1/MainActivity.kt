98af420930fbe21effd7aaf359722d6c5f223482
Hajar Salamah <programer4455@gmail.com>
2020-09-20 08:50:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/geoquiz_1/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.geoquiz_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var  trueButton: Button
+    private lateinit var falseButton:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton=findViewById(R.id.true_button)
+        falseButton=findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener{
+         var t= Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_SHORT)
+                 t.setGravity(Gravity.TOP,0,0)
+                         t.show()
+
+        }
+        falseButton.setOnClickListener{
+           var f= Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_SHORT)
+            f.setGravity(Gravity.TOP,0,0)
+               f.show()
+        }
+    }
+}
